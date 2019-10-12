6bbe820eefa91b56056a2612f9fb171655b23dab
Mihir Chaturvedi <plibither8@gmail.com>
2019-10-12 17:28:27 +0530

    IP Ass 3

---
+++ assignments/3/Mihir Chaturvedi_2019061.py
@@ -0,0 +1,155 @@
+"""
+CSE101: Introduction to Programming
+Assignment 3
+
+Name        : Mihir Chaturvedi
+Roll-no     : 2019061
+"""
+
+
+
+import math
+import random
+
+
+
+def dist(p1, p2):
+    """
+    Find the euclidean distance between two 2-D points
+
+    Args:
+        p1: (p1_x, p1_y)
+        p2: (p2_x, p2_y)
+    
+    Returns:
+        Euclidean distance between p1 and p2
+    """
+    pass
+
+
+
+def sort_points_by_X(points):
+    """
+    Sort a list of points by their X coordinate
+
+    Args:
+        points: List of points [(p1_x, p1_y), (p2_x, p2_y), ...]
+    
+    Returns:
+        List of points sorted by X coordinate
+    """
+    pass
+
+
+
+def sort_points_by_Y(points):
+    """
+    Sort a list of points by their Y coordinate
+
+    Args:
+        points: List of points [(p1_x, p1_y), (p2_x, p2_y), ...]
+    
+    Returns:
+        List of points sorted by Y coordinate 
+    """
+    pass
+
+
+
+def naive_closest_pair(plane):
+    """
+    Find the closest pair of points in the plane using the brute
+    force approach
+
+    Args:
+        plane: List of points [(p1_x, p1_y), (p2_x, p2_y), ...]
+
+    Returns:
+        Distance between closest pair of points and closest pair 
+        of points: [dist_bw_p1_p2, (p1_x, p1_y), (p2_x, p2_y)]
+    """
+    pass    
+
+
+
+def closest_pair_in_strip(points, d):
+    """
+    Find the closest pair of points in the given strip with a 
+    given upper bound. This function is called by 
+    efficient_closest_pair_routine
+
+    Args:
+        points: List of points in the strip of interest.
+        d: Minimum distance already found found by 
+            efficient_closest_pair_routine
+
+    Returns:
+        Distance between closest pair of points and closest pair 
+        of points: [dist_bw_p1_p2, (p1_x, p1_y), (p2_x, p2_y)] if
+        distance between p1 and p2 is less than d. Otherwise
+        return -1.
+    """
+    pass
+
+
+
+def efficient_closest_pair_routine(points):
+    """
+    This routine calls itself recursivly to find the closest pair of
+    points in the plane. 
+
+    Args:
+        points: List of points sorted by X coordinate
+
+    Returns:
+        Distance between closest pair of points and closest pair 
+        of points: [dist_bw_p1_p2, (p1_x, p1_y), (p2_x, p2_y)]
+    """
+    pass
+
+
+
+def efficient_closest_pair(points):
+    """
+    Find the closest pair of points in the plane using the divide
+    and conquer approach by calling efficient_closest_pair_routine.
+
+    Args:
+        plane: List of points [(p1_x, p1_y), (p2_x, p2_y), ...]
+
+    Returns:
+        Distance between closest pair of points and closest pair 
+        of points: [dist_bw_p1_p2, (p1_x, p1_y), (p2_x, p2_y)]
+    """
+    pass
+
+
+
+def generate_plane(plane_size, num_pts):
+    """
+    Function to generate random points.
+
+    Args:
+        plane_size: Size of plane (X_max, Y_max)
+        num_pts: Number of points to generate
+
+    Returns:
+        List of random points: [(p1_x, p1_y), (p2_x, p2_y), ...]
+    """
+    
+    gen = random.sample(range(plane_size[0]*plane_size[1]), num_pts)
+    random_points = [(i%plane_size[0] + 1, i//plane_size[1] + 1) for i in gen]
+
+    return random_points
+
+
+
+if __name__ == "__main__":  
+    #number of points to generate
+    num_pts = 10
+    #size of plane for generation of points
+    plane_size = (10, 10) 
+    plane = generate_plane(plane_size, num_pts)
+    print(plane)
+    #naive_closest_pair(plane)
+    #efficient_closest_pair(plane)
