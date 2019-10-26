f2de8a76d63328fc27d4167e4e61d45f9b874c8f
Mihir Chaturvedi <plibither8@gmail.com>
2019-10-27 00:03:24 +0530

    Fix: Remove unused colors

--- assignments/4/2019061_a4.py
+++ assignments/4/2019061_a4.py
@@ -6,13 +6,11 @@ def clearScreen():
     os.system('clear' if os.name is 'posix' else 'cls')
 
 class Color:
-    BLACK = '\u001b[30;1m'
     RED = '\u001b[31;1m'
     GREEN = '\u001b[32;1m'
     YELLOW = '\u001b[33;1m'
     BLUE = '\u001b[34;1m'
     MAGENTA = '\u001b[35;1m'
-    CYAN = '\u001b[36;1m'
     WHITE = '\u001b[37;1m'
     RESET = '\u001b[0m'
 
